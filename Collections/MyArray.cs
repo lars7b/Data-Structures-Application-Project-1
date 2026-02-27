421633f40b679341caeae39b9024cc9bb28f1171
Githubjael <74548587+Githubjael@users.noreply.github.com>
2026-02-27 10:18:27 +0100

    add MyArray class implementing IMyCollection interface

---
+++ Collections/MyArray.cs
@@ -0,0 +1,58 @@
+public class MyArray<T> : IMyCollection<T>
+{
+    private T[] _array;
+    private int _count;
+    public MyArray()
+    {
+        _array = new T[4];
+        _count = 0;
+    }
+    public int Count => _count;
+
+    public bool Dirty { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public void Add(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IMyCollection<T> Filter(Func<T, bool> predicate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T FindBy<K>(K key, Func<T, K, bool> comparer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IMyIterator<T> GetIterator()
+    {
+        throw new NotImplementedException();
+    }
+
+    public R Reduce<R>(Func<R, T, R> accumulator)
+    {
+        throw new NotImplementedException();
+    }
+
+    public R Reduce<R>(R initial, Func<R, T, R> accumulator)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Remove(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Sort(Comparison<T> comparison)
+    {
+        throw new NotImplementedException();
+    }
+}
