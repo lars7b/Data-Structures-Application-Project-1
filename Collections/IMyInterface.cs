cd65071e49aeb09c67f573bfbeabcafa83898b9f
Day <night-deploy@outlook.com>
2026-03-02 21:11:16 +0100

    Feat: Implement MyListIterator.cs,  GetEnumerator() and GetIterator() + clean up a bit of code.

--- Collections/IMyInterface.cs
+++ Collections/IMyInterface.cs
@@ -9,10 +9,7 @@ public interface IMyCollection<T>
     T FindBy<K>(K key, Func<T, K, bool> comparer);
     IMyCollection<T> Filter(Func<T, bool> predicate);
     void Sort(Comparison<T> comparison);
-
     R Reduce<R>(Func<R, T, R> accumulator);
-
-    //OR
     R Reduce<R>(R initial, Func<R, T, R> accumulator);
     IMyIterator<T> GetIterator(); //todo: make custom Iterator, cant use System.Collections.Generic
     IEnumerator<T> GetEnumerator();
--- Collections/MyList.cs
+++ Collections/MyList.cs
@@ -1,25 +1,34 @@
+using System.Collections;
+
 namespace Project_1.Collections;
 
-public class MyList<T> : IMyCollection<T>
+public class MyList<T> : IMyCollection<T>, IEnumerable<T> //TODO: Ask if allowed to inherit IEnumerable<T>
 {
     private T[] _array;
-    private int _count;
 
-    public int Count => _count;
+    public int Count { get; private set; }
 
     public bool Dirty { get; set; }
 
     public MyList()
     {
-        _array = new T[4];
-        _count = 0;
+        _array = [];
+        Count = 0;
+    }
+
+    public MyList(IEnumerable<T> collection)
+    {
+        _array = [];
+        Count = 0;
+        foreach (var item in collection)
+            Add(item);
     }
 
     public void Add(T item)
     {
-        if (_count == _array.Length) Resize(_array, _count * (3 / 2));
-        _array[_count] = item;
-        _count++;
+        if (Count == _array.Length) Resize(_array, Count + 1);
+        _array[Count] = item;
+        Count++;
         Dirty = true;
     }
 
@@ -27,7 +36,7 @@ public class MyList<T> : IMyCollection<T>
     {
         var result = new MyList<T>();
 
-        for (var i = 0; i < _count; i++)
+        for (var i = 0; i < Count; i++)
             if (predicate(_array[i]))
                 result.Add(_array[i]);
         return result;
@@ -40,12 +49,18 @@ public class MyList<T> : IMyCollection<T>
 
     public IEnumerator<T> GetEnumerator()
     {
-        throw new NotImplementedException();
+        for (var i = 0; i < Count; i++)
+            yield return _array[i];
+    }
+    
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
     }
 
     public IMyIterator<T> GetIterator()
     {
-        throw new NotImplementedException();
+        return new MyListIterator<T>(_array);
     }
 
     public R Reduce<R>(Func<R, T, R> accumulator)
@@ -68,7 +83,7 @@ public class MyList<T> : IMyCollection<T>
                 {
                     _array[j] = _array[j + 1];
                 }
-                _count--;
+                Count--;
             }
         }
     }
