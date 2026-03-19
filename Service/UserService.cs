3402a8e2a3b070f7f7e4ab3385a41811eba38283
Githubjael <74548587+Githubjael@users.noreply.github.com>
2026-03-19 14:17:43 +0100

    Refactor: Remove redundant Admin user initialization in UserService constructor

--- Service/UserService.cs
+++ Service/UserService.cs
@@ -13,9 +13,6 @@ public class UserService : IUserService<User>
     {
         _repository = repository;
         _users = _repository.LoadAllUsers();
-        var name = "Admin";
-        var result = _users.FindBy(name, (name, key) => name.Name == "Admin");
-        if (result == null) _users.Add(new Admin());
         _loggedIn = false;
     }
 
