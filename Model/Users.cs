056696cee95c42fae6e9ca32cd834a6aa50c9970
Githubjael <74548587+Githubjael@users.noreply.github.com>
2026-03-28 12:34:30 +0100

    Refactor User class to encapsulate properties and add role management

--- Model/Users.cs
+++ Model/Users.cs
@@ -1,34 +1,39 @@
+public enum Access
+{
+    Basic = 0,
+    Admin = 1
+}
+
 public class User
 {
-    public int Id{get;set;}
+    public int Id{get;private set;}
     private string _name;
     private string _password;
+    private Access _role;
+    public Access Role{get => _role;}
     public string Name{get => _name;}
-    public string Password{get => _password;}
-    public User(string name, string password)
+    public User(int id, string name, string password)
     {
+        Id = id;
         _name = name;
         _password = password;
+        _role = Access.Basic;
     }
     public bool ChangeName(string name)
     {
+        if(string.IsNullOrWhiteSpace(name)) return false;
         _name = name;
         return true;
     }
     public bool ChangePassword(string o_passw, string n_passw)
     {
-        if(Password == o_passw)
+        if(string.IsNullOrWhiteSpace(o_passw)) return false;
+        if(_password == o_passw)
         {
+            if(string.IsNullOrWhiteSpace(n_passw)) return false;
             _password = n_passw;
             return true;
         }
         return false;
-    }
-}
-
-public class Admin : User
-{
-    public Admin(string name, string password) : base(name, password)
-    {
     }
 }
